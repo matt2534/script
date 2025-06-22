34ca521d385265cbc80072ff23c124defcf07ed7
Matthew <77067215+matt2534@users.noreply.github.com>
2025-06-23 00:33:17 +0200

    Create loader.js

---
+++ loader.js
@@ -0,0 +1,11 @@
+// ad-loader.js
+(function () {
+  var container = document.getElementById("container-55bd0119beed62c3dd53dbf1f316dc95");
+  if (!container) return;
+
+  var script = document.createElement("script");
+  script.async = true;
+  script.setAttribute("data-cfasync", "false");
+  script.src = "//stallsobservance.com/55bd0119beed62c3dd53dbf1f316dc95/invoke.js";
+  container.appendChild(script);
+})();
